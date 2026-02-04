2bcc79947a583077637ae66ba3162ee487b3082f
lisovskyi-arsenii <lisovskyiarsenii@gmail.com>
2026-02-04 06:16:33 +0200

    commit

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,8 +3,6 @@ package main;
 import core.BooleanSearchEngine;
 import lombok.extern.slf4j.Slf4j;
 import menu.MenuController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import scanner.CustomScanner;
 
 import java.time.LocalDateTime;
